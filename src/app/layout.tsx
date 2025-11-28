bf30db2399b0d1d7a311729dbc9957b135e3f8e4
Devhypertech <dev.hypertechverse@gmail.com>
2025-11-29 01:02:13 +0500

    Add Tawk.to chat widget integration to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Header from "../../components/layout/header";
 import Footer from "../../components/layout/footer";
@@ -15,6 +16,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         {children}
         <Footer />
+        {/* Tawk.to Script */}
+        <Script
+          id="tawkto-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/663d1b479a809f19fb2f2787/1htfb54no';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
